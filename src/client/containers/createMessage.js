acf575b7792e2ade2df15acc869253d40c4765cd
Teo Fleming <mokolodi1@gmail.com>
2017-01-26 15:15:20 -0800

    add createMessage form

--- src/client/containers/createMessage.js
+++ src/client/containers/createMessage.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import _ from "underscore"
+import { addMessage } from "../actions/addMessage"
 
-const CreateMessage = () => {
+const CreateMessage = ({ dispatch }) => {
+  let input
+
   return (
     <div>
-      <form action="">
+      <form onSubmit={e => {
+        e.preventDefault()
+        if (!input.value.trim()) {
+          return
+        }
+        dispatch(addMessage(input.value))
+        input.value = ''
+      }}>
         Message:<br/>
-        <input type="text" name="newMessage"/>
+      <input type="text" ref={node =>
+          input = node
+        }/>
         <input type="submit" value="Submit"/>
       </form>
     </div>
   );
 }
 
-export default CreateMessage
+export default connect()(CreateMessage)
