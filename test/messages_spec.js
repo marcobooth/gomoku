6ebfd627c804c5b2bd2a7364f0f49f9869a3bd0b
Marco BOOTH <marcobooth@hotmail.com>
2017-02-09 13:13:04 -0800

    commit before testing

---
+++ test/messages_spec.js
@@ -0,0 +1,22 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import {
+//   renderIntoDocument,
+//   scryRenderedDOMComponentsWithClass
+// } from 'react-addons-test-utils';
+// import Messages from '../src/client/containers/messages';
+// import {expect} from 'chai';
+// import Immutable from 'immutable'
+//
+// describe('Board', () => {
+//
+//   it('renders', () => {
+//     const component = renderIntoDocument(
+//       <Messages />
+//     );
+//
+//     const rows = scryRenderedDOMComponentsWithClass(component, 'row');
+//     expect(rows.length).to.equal(boardInput.size);
+//   });
+//
+// });
