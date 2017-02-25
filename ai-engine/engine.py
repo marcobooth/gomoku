65ed0c55a5e9d9cd4f14c4b87f5c271b658520df
Teo Fleming <mokolodi1@gmail.com>
2017-02-24 20:17:31 -0800

    working on engine

--- ai-engine/engine.py
+++ ai-engine/engine.py
@@ -1,19 +1,69 @@
-class Board(object):
-	def __init__(self, oldBoard, row, col, player):
-		# create new board/game state from old board and move
-		# have a constant starting board delcared somewhere
+from copy import copy
 
-	def heuristic():
+BOARD_SIZE = 19
+
+class Board(object):
+	# TODO: do we need to take depth into account for the heuristic?
+	def __init__(self, board, inPlayCells=0, maxPlayerThreats=0, minPlayerThreats=0, maximizingPlayer=True):
+		# set default inPlayCells if necessary
+		if inPlayCells == 0:
+			inPlayCells = {}
+
+			for i in range(BOARD_SIZE):
+				inPlayCells[i] = {}
+
+		# calculate min/max threats if necessary
+		if maxPlayerThreats == 0:
+			# I AM HERE
+
+		this.board = board
+		this.inPlayCells = inPlayCells
+		this.maxPlayerThreats = maxPlayerThreats
+		this.minPlayerThreats = minPlayerThreats
+
+	def move(self, row, col, blackOrWhite):
+		# copy over the board, reusing as much memory as possible
+		newBoard = copy(oldBoard.board)
+		newBoard[row] = copy(oldBoard.board[row])
+		newBoard[row][col] = blackOrWhite
+
+		# add to the in play cells
+		inPlayCells = copy(this.inPlayCells)
+
+		# remove the place we just played
+		del inPlayCells[row][col]
+
+		# add the cells around where the move was
+		for currentRow in range(row - 2, row + 3):
+			if row < 0 or row > BOARD_SIZE: continue
+
+			for currentCol in range(col - 2, col + 3):
+				if col < 0 or col > BOARD_SIZE: continue
+
+				# add the current row/col to the in play cells
+				if not board[currentRow][currentCol]:
+					inPlayCells[currentRow][currentCol] = True
+
+		minPlayerThreats = this.minPlayerThreats
+		maxPlayerThreats = this.maxPlayerThreats
+		# TODO: check to see if we need to add any threats
+
+		return Board(newBoard, maxPlayerThreats, minPlayerThreats)
+
+	def heuristic(self):
+		# TODO: return something about the threats
 		# return the heuristic for this board
 
-	def getChildren():
+	def getChildren(self):
+		return [move(self, row, col,
+
+		for yop in a:
+...     for hi in a[yop]:
+...             print str(yop) + " " + str(hi)
+		return []
 		# return a bunch of new boards that are children of this board
 
-class Game(object):
-	def __init__(self):
-		# ?
-
-	def alphabeta(node, depth, alpha, beta, maximizingPlayer):
+	def alphabeta(self, depth, alpha, beta, maximizingPlayer):
 		if depth == 0 or hasWinner(node):
 			return calculateHeuristic(node)
 
@@ -33,3 +83,5 @@ class Game(object):
 				if (beta <= alpha):
 					break
 			return value
+
+board = Board([['' for i in range(BOARD_SIZE)] for i in range(BOARD_SIZE)])
