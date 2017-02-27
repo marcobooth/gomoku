ddfcab81b36eb96a20171678e551bfcf5584bcab
Marco BOOTH <marcobooth@hotmail.com>
2017-02-27 10:38:43 -0800

    checks winning scenarios

--- src/server/core.js
+++ src/server/core.js
@@ -1,8 +1,20 @@
 import Immutable from 'immutable';
 import { List, Map } from 'immutable';
+import { verticalWinningState, horizontalWinningState, rightDiagonalWinningState, leftDiagonalWinningState }
 
-export function startGame(state) {
-  return state.update(['test'], value => { return true })
+function checkForWinner(state) {
+  var currentPlayer = state.get("player")
+  var board = state.get("board").toJS()
+  for (var i = 0; i < board.length; i++) {
+    for(var j = 0; j < board[i].length; j++) {
+      if (verticalWinningState(board, currentPlayer, [i, j])      ||
+          horizontalWinningState(board, currentPlayer, [i, j])    ||
+          rightDiagonalWinningState(board, currentPlayer, [i, j]) ||
+          leftDiagonalWinningState(board, currentPlayer, [i, j])) {
+            console.log("winner");
+          }
+    }
+  }
 }
 
 function changePlayer(state) {
@@ -11,8 +23,9 @@ function changePlayer(state) {
 }
 
 export function placePiece(state, mainKey, secondKey) {
-  let current_colour = state.get("player") == 1 ? "black" : "red"
-  let newState = state.updateIn(['board', mainKey, secondKey], colour => { return current_colour })
+  let currentColour = state.get("player") == 1 ? "black" : "red"
+  let newState = state.updateIn(['board', mainKey, secondKey], colour => { return currentColour })
+  checkForWinner(newState)
   return changePlayer(newState)
 }
 
