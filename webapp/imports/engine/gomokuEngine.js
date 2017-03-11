f10840ddf6b2e9d2eb18835cd49eac3976825aea
Teo Fleming <mokolodi1@gmail.com>
2017-03-10 22:40:19 -0800

    got another test working; need to work on merging threats on move

--- webapp/imports/engine/gomokuEngine.js
+++ webapp/imports/engine/gomokuEngine.js
@@ -1,12 +1,15 @@
-// generate the default inPlayCells once
+export const BOARD_SIZE = 19
+
+// generate some default values
 const defaultInPlay = {}
-_.times(19, (index) => {
+_.times(BOARD_SIZE, (index) => {
   defaultInPlay[index] = {}
 })
 
-class ThreatFinder {
-  constructor(description, step)
-}
+const defaultCellToThreats = Array(BOARD_SIZE);
+_.times(BOARD_SIZE, (index) => {
+  defaultCellToThreats[index] = Array(BOARD_SIZE)
+})
 
 // define in which ways threats can be found
 // NOTE: step functions modify the passed object
@@ -22,13 +25,14 @@ const threatFinders = [
 ]
 
 export class Board {
-  constructor(values, maximizingPlayer = true, inPlayCells = defaultInPlay,
-        threats, cellToThreats) {
+  // NOTE: player === true means that the player is the maximizing player
+  constructor(values, player = true, inPlayCells = defaultInPlay,
+        threats = [], cellThreats = defaultCellToThreats) {
     this.values = values
-    this.maximizingPlayer = maximizingPlayer
+    this.player = player
     this.inPlayCells = inPlayCells
     this.threats = threats
-    this.cellToThreats = cellToThreats
+    this.cellThreats = cellThreats
   }
 
   static newThreat(player, finderIndex, location) {
@@ -43,26 +47,65 @@ export class Board {
       skipped: [],
 
       // expansion locations on either end
-      expansions: []
+      expansions: [],
 
       // the size of the threat based on just what's been played
       span: 1,
     }
   }
 
-  static addCellToThreats(cellToThreats, threat) {
-    // TODO
+  // add expansions, add the threat to the threat list, link to it through
+  // toThreats
+  static addThreat(newThreats, newToThreats, oldToThreats, threat, values) {
+    // add expansions to the threat
+    if (threat.span < 5) {
+      let possibleExtensions = [
+        { delta: -1, playedIndex: 0 },
+        { delta: 1, playedIndex: threat.played.length - 1 },
+      ]
+
+      _.each(possibleExtensions, ({ delta, playedIndex }) => {
+        let location = _.clone(threat.played[playedIndex])
+        threatFinders[threat.finderIndex](location, delta)
+
+        // XXX: what happens when the board is the current player there?
+        if (!Board.outsideBoard(location) &&
+            values[location.row][location.col] === null) {
+          threat.expansions.push(location)
+        }
+      })
+    }
+
+    newThreats.push(threat)
+
+    // TODO: add to the toThreats thing
+  }
+
+  // sort by column then row
+  // NOTE: order important in addThreat when adding extension information
+  // because of the way deltas work with the current implementation of
+  // threatFinders
+  static compareLocations(first, second) {
+    if (first.col != second.col) return first.col - second.col
+    if (first.row != second.row) return first.row - second.row
+    return 0
+  }
+
+  static outsideBoard({ row, col }) {
+    return row < 0 || row >= BOARD_SIZE || col < 0 || col >= BOARD_SIZE;
   }
 
   move({ row, col }) {
     // create new values for everything, reusing as much memory as possible
     let newValues = this.values.slice();
     newValues[row] = this.values[row].slice()
-    let player = !this.maximizingPlayer
     let newThreats = this.threats.slice();
+    let newCellThreats = Object.assign({}, this.cellThreats)
+
+    // console.log(`\nmoving ${this.player}: (${row}, ${col})`)
 
     // put the piece down
-    newValues[row][col] = maximizingPlayer
+    newValues[row][col] = this.player
 
     // TODO: remove any captured pieces, figure out if we need to remove any
     // threats due to removal
@@ -72,73 +115,98 @@ export class Board {
 
     // figure out if we need to add any threats for the current player
     for (finderIndex in threatFinders) {
+      // console.log(`finderIndex: ${finderIndex}`)
       let stepCell = threatFinders[finderIndex]
 
       // go backwards and then forwards, adding to the threat in each direction,
       // and then do the reverse of all that
       let deltas = [ -1, 1 ]
+      let addThreats = []
 
-      let directionalThreats = _.map([ false, true ], (firstBit) => {
-        let threat = newThreat(player, finderIndex, { row, col })
+      _.each([ false, true ], (firstBit) => {
+        let threat = Board.newThreat(this.player, finderIndex,
+            { row, col })
 
         // go the other way the second time around
         for (secondBit of [ false, true ]) {
           let current = { row, col }
           let skipped = []
+          let blocked = false
 
-          for (let i = 0; (i < 5) && (threat.span + skipped.length < 5); i++) {
+          for (i = 0; (i < 5) && (threat.span + skipped.length < 5); i++) {
             // clever, eh? http://stackoverflow.com/a/3618366
             stepCell(current, deltas[firstBit ^ secondBit])
+            // console.log(`after step of ${deltas[firstBit ^ secondBit]}: (${current.row}, ${current.col})`)
 
             // check to see if we're still on the board
-            if (outsideBoard(current)) {
+            if (Board.outsideBoard(current)) {
               break
             }
 
             let value = newValues[current.row][current.col]
-            if (value === player) {
+            if (value === this.player) {
               // it's this player's
 
               // TODO: if the cell we're looking at is part of a threat with
               // this finder, check to see if this should join that threat.
 
-              // if we skipped any to get here add those too
-              // NOTE: threat.played order matters in combineThreats
-              threat.played.push(current)
+              threat.played.push(_.clone(current))
               threat.skipped = threat.skipped.concat(skipped)
               threat.span += 1 + skipped.length
               skipped = []
             } else if (value === null) {
               // it's an empty spot
-              skipped.push(current)
+              skipped.push(_.clone(current))
             } else {
               // it's the other player's -- time to quit this direction
+              blocked = true
               break
             }
           }
         }
 
-        return threat
+        addThreats.push(threat)
       })
 
-      // check to see if the directional threats are the same
-      let combinedThreat = combineThreats(directionalThreats)
+      // if the first's span is 1, so is the second's
+      if (addThreats[0].span === 1) continue
 
-      if (combinedThreat) {
-        newThreats.push(combinedThreat)
-        addCellToThreats(newCellToThreats, combinedThreat)
-      } else {
-        // add both threats separately
-        _.each(directionalThreats, (threat) => {
-          newThreats.push(threat)
-          addCellToThreats(newCellToThreats, threat)
-        })
+      Board.addThreat(newThreats, newCellThreats, this.cellThreats,
+          addThreats[0], newValues)
+
+      // check to see if the directional threats are the same
+      if (!_.isEqual(addThreats[0].played, addThreats[1].played)) {
+        Board.addThreat(newThreats, newCellThreats, this.cellThreats,
+            addThreats[1], newValues)
       }
     }
 
-    // TODO: update the in play cells
+    // update the in play cells
+    let newInPlay = Object.assign({}, this.inPlayCells);
+    newInPlay[row] = Object.assign({}, this.inPlayCells[row])
 
-    return new Board(newValues, player, newInPlay, newThreats, newCellToThreats)
+    // remove this cell
+    delete newInPlay[row][col]
+
+    // add the cells around it
+    for (let r = row - 2; r <= row + 2; r++) {
+      if (r < 0 || r >= BOARD_SIZE) continue
+
+      for (let c = col - 2; c <= col + 2; c++) {
+        // only add the cell if it's within bounds and blank
+        if (c < 0 || c >= BOARD_SIZE || newValues[r][c] === null) continue
+
+        // don't change the parent's information
+        if (newInPlay[r] === this.inPlayCells[r]) {
+          newInPlay[r] = Object.assign({}, this.inPlayCells[r])
+        }
+
+        newInPlay[r][c] = true
+      }
+    }
+
+    return new Board(newValues, !this.player, newInPlay, newThreats,
+        newCellThreats)
   }
 
   static scoreThreat(threat) {
--- webapp/imports/engine/gomokuEngine.test.js
+++ webapp/imports/engine/gomokuEngine.test.js
@@ -1,11 +1,11 @@
 import _ from "underscore"
 import { chai } from 'meteor/practicalmeteor:chai';
 
-import { Board, getBestMove } from "./gomokuEngine"
+import { Board, BOARD_SIZE, getBestMove } from "./gomokuEngine"
 
 var blankBoard = []
-_.times(19, () => {
-  let row = Array(19)
+_.times(BOARD_SIZE, () => {
+  let row = Array(BOARD_SIZE)
   _.times(row.length, (index) => {
     row[index] = null
   })
@@ -13,43 +13,107 @@ _.times(19, () => {
   blankBoard.push(row)
 })
 
+var { assert } = chai
+
 describe('Gomoku engine', function () {
   //                      0123456789
   it('moves from scratch: ..1..43..2.........', function () {
     let startBoard = new Board(blankBoard)
 
-    chai.assert(startBoard.getThreats().length === 0)
+    assert(startBoard.getThreats().length === 0)
 
     let row = 7
-    let board = startBoard.move(row, 2).move(0, 0)
-    chai.assert(startBoard.getThreats().length === 0)
-    chai.assert(board.getThreats().length === 0)
+    let board = startBoard.move({ row, col: 2 }).move({ row: 0, col: 0 })
+    assert(startBoard.getThreats().length === 0)
+    assert(board.getThreats().length === 0)
 
     // TODO: check in play cells
     // TODO: check we put the piece in the right place,
     // didn't modify startBoard
 
-    board = board.move(row, 9).move(0, 10)
-    chai.assert(board.getThreats().length === 0)
+    board = board.move({ row, col: 9 }).move({ row: 0, col: 10 })
+    assert(board.getThreats().length === 0)
 
-    board = board.move(row, 6).move(0, 19)
+    board = board.move({ row, col: 6 }).move({ row: 0, col: 19 })
     let threats = board.getThreats()
-    chai.assert(threats.length === 2)
-    chai.assert(_.isEqual(threats[0], {
+    assert.equal(threats.length, 2)
+    assert.deepEqual(threats[0], {
       player: true,
-      finderIndex: 1,
+      finderIndex: "1",
       played: [ { row, col: 6 }, { row, col: 2 } ],
-      skipped: [ { row, col: 5 }, { row, col: 4 }, { row, col: 3 } ]
+      skipped: [ { row, col: 5 }, { row, col: 4 }, { row, col: 3 } ],
       expansions: [],
       span: 5,
-    }))
-    chai.assert(_.isEqual(threats[0], {
+    })
+    console.log(threats[1])
+    assert.deepEqual(threats[1], {
       player: true,
-      finderIndex: 1,
+      finderIndex: "1",
       played: [ { row, col: 6 }, { row, col: 9 } ],
-      skipped: [ { row, col: 7 }, { row, col: 8 } ]
+      skipped: [ { row, col: 7 }, { row, col: 8 } ],
       expansions: [ { row, col: 5 }, { row, col: 10 } ],
       span: 4,
-    }))
+    })
+
+    board = board.move({ row, col: 5 })
+    threats = board.getThreats()
+    assert.equal(threats.length, 2)
+    console.log(threats[0])
+    assert.deepEqual(threats[0], {
+      player: true,
+      finderIndex: "1",
+      played: [ { row, col: 6 }, { row, col: 2 }, { row, col: 5 } ],
+      skipped: [ { row, col: 4 }, { row, col: 3 } ],
+      expansions: [],
+      span: 5,
+    })
+    console.log(threats[1])
+    assert.deepEqual(threats[1], {
+      player: true,
+      finderIndex: "1",
+      played: [ { row, col: 6 }, { row, col: 9 }, { row, col: 5 } ],
+      skipped: [ { row, col: 7 }, { row, col: 8 } ],
+      expansions: [ { row, col: 10 } ],
+      span: 5,
+    })
+  })
+
+  //                      01234567890123456789
+  it('moves from scratch: .......1234.........', function () {
+    let board = new Board(blankBoard)
+
+    let row = 7;
+    board = board.move({ row, col: 7 }).move({ row: 0, col: 0 })
+    assert.equal(board.getThreats().length, 0)
+
+    board = board.move({ row, col: 8 }).move({ row: 0, col: 10 })
+    assert.deepEqual(board.getThreats()[1], {
+      player: true,
+      finderIndex: "1",
+      played: [ { row, col: 7 }, { row, col: 8 } ],
+      skipped: [],
+      expansions: [ { row, col: 6 }, { row, col: 9 } ],
+      span: 2,
+    })
+
+    board = board.move({ row, col: 8 }).move({ row: 0, col: 19 })
+    assert.deepEqual(board.getThreats()[0], {
+      player: true,
+      finderIndex: "1",
+      played: [ { row, col: 7 }, { row, col: 8 }, { row, col: 9 } ],
+      skipped: [],
+      expansions: [ { row, col: 6 }, { row, col: 10 } ],
+      span: 3,
+    })
+
+    board = board.move({ row, col: 9 }).move({ row: 19, col: 0 })
+    assert.deepEqual(board.getThreats()[1], {
+      player: true,
+      finderIndex: "1",
+      played: [ { row, col: 7 }, { row, col: 8 }, { row, col: 9 }, { row, col: 10 } ],
+      skipped: [],
+      expansions: [ { row, col: 6 }, { row, col: 11 } ],
+      span: 4,
+    })
   })
 })
